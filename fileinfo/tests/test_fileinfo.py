1ba272d0a9384ba784fc30018e51b57d8ee54a1d
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-05-24 13:34:49 +0000

    patch-with-context-manager

--- fileinfo/tests/test_fileinfo.py
+++ fileinfo/tests/test_fileinfo.py
@@ -2,7 +2,9 @@
 
 """Tests for `fileinfo` package."""
 
-from fileinfo.fileinfo import fileinfo
+from fileinfo.fileinfo import FileInfo
+from unittest.mock import patch
+
 
 def test_init():
     filename = "somefile.ext"
@@ -15,3 +17,14 @@ def test_init_relative():
     relative_path = "../{}".format(filename)
     fi = FileInfo(relative_path)
     assert fi.filename == filename
+
+
+def test_get_info():
+    filename = "somefile.ext"
+    original_path = "../{}".format(filename)
+
+    with patch("os.path.abspath") as abspath_mock:
+        test_abspath = "some/abs/path"
+        abspath_mock.return_value = test_abspath
+        fi = FileInfo(original_path)
+        assert fi.get_info() == (filename, original_path, test_abspath)
