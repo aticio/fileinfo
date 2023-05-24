073fdcc090ba351e8809f3f29a81afee6cac9546
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-05-24 14:43:08 +0000

    multiple-patches

--- fileinfo/fileinfo/fileinfo.py
+++ fileinfo/fileinfo/fileinfo.py
@@ -6,10 +6,11 @@ class FileInfo:
     def __init__(self, path):
         self.original_path = path
         self.filename = os.path.basename(path)
-    
+
     def get_info(self):
         return (
             self.filename,
             self.original_path,
-            os.path.abspath(self.original_path)
+            os.path.abspath(self.original_path),
+            os.path.getsize(self.filename)
         )
